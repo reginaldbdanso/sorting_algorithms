4a47c53781e3211b7b5f6a6ee5f1c5e6914e80bb
Nutifafa Foli-Biaku <nutifafa.foli@gmail.com>
2023-07-25 18:18:41 +0000

    implemented bubble sort algo for task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * swap - swaps positions of two numbers
+ * @a: pointer to first number to be swapped
+ * @b: pointer to second number to be swapped
+*/
+void swap(int *a, int *b)
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
+ * bubble_sort - sort an array of integers in an ascending order
+ * @array: array of integers to be sorted
+ * @size: size of the array to be sorted
+ * Description: prints the array after every swap
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - 2; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
+			}
+		}
+	}
+}
